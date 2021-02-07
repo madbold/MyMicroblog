2390b0d6886c99e84bffd4893f6dc2d2e72339d2
vinod <vinodyadav396@gmail.com>
2021-02-07 17:55:18 +0530

    added config file

--- config.py
+++ config.py
@@ -10,9 +10,12 @@ class Config(object):
     '''
     classdocs
     '''
-    SECRET_KEY = 'abcdef'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POSTS_PER_PAGE=3
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
-        
+    POSTS_PER_PAGE = 3
